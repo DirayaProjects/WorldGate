0bc36adede918ee2812051740f4ed3de679e04c3
HassanShahrour <hassanshahrour04@gmail.com>
2025-03-11 01:49:19 +0200

    init

--- Extensions/InterfaceScopeExtensions.cs
+++ Extensions/InterfaceScopeExtensions.cs
@@ -4,25 +4,25 @@ using WebPortal.Utilities;
 
 namespace WebPortal.Extensions
 {
-	public static class InterfaceScopeExtensions
-	{
-		public static IServiceCollection AddInterfaceScopeServices(this IServiceCollection services, IConfiguration configuration)
-		{
+    public static class InterfaceScopeExtensions
+    {
+        public static IServiceCollection AddInterfaceScopeServices(this IServiceCollection services, IConfiguration configuration)
+        {
 
-			#region Scops
+            #region Scops
 
-			services.AddScoped<FirebaseUtility>();
-			services.AddScoped<FirebaseStorageUtility>();
-			services.AddSingleton<FirebaseUtility>();
-			services.AddHostedService<PlanExpirationChecker>();
-			services.AddScoped<IUserRepository, UserRepository>();
-			services.AddScoped<IChannelRepository, ChannelRepository>();
-			services.AddScoped<ICodeRepository, CodeRepository>();
-			services.AddScoped<IPlanRepository, PlanRepository>();
+            services.AddScoped<FirebaseUtility>();
+            services.AddScoped<FirebaseStorageUtility>();
+            services.AddSingleton<FirebaseUtility>();
+            services.AddHostedService<PlanExpirationChecker>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IChannelRepository, ChannelRepository>();
+            services.AddScoped<ICodeRepository, CodeRepository>();
+            services.AddScoped<IPlanRepository, PlanRepository>();
 
-			#endregion
+            #endregion
 
-			return services;
-		}
-	}
+            return services;
+        }
+    }
 }
